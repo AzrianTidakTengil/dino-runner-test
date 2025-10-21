7364afce8caf5b9b01452cff8112ac4978732331
Gregori Paul <gregoripaulll@gmail.com>
2025-10-21 22:16:31 +0700

    Add some music, sprite, and some logic

--- main.ts
+++ main.ts
@@ -2,46 +2,177 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     if (mySprite.vy == 0) {
         mySprite.vy = -150
     }
+    music.play(music.createSoundEffect(WaveShape.Square, 400, 600, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    mySprite.setImage(assets.image`myImage2`)
     game.gameOver(true)
     game.setGameOverMessage(true, "GAME OVER!")
     game.setGameOverScoringType(game.ScoringType.HighScore)
 })
-let projectile: Sprite = null
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(img`
+        ........................
+        ........................
+        ........................
+        ...........ccc..........
+        ...........cccc.........
+        .......ccc..ccccccc.....
+        .......cccccc555555cc...
+        ........ccb5555555555c..
+        .....cc..b555555555555c.
+        .....cccb55555bcc555555c
+        ......cb555555555c55d55c
+        ......b5555555555555555c
+        ...cc.b555dd5555bb1bbbc.
+        ....ccd55ddddd55bbb335c.
+        ...ccbddddddddd5bb3335c.
+        .ccccdddddddddd55b3335c.
+        cdcccdddddb55bbd553335c.
+        cddbddddddb555bb55555c..
+        cddddddddddb5555bbccc...
+        ccddddddbd55bb55cbc.....
+        .ccddddbbbdd55ccbdc.....
+        ...ccbbbcbddddccdddc....
+        .....ccccdd555dccccc....
+        ........cccccccc........
+        `)
+})
+let Bat: Sprite = null
+let Cactus: Sprite = null
 let mySprite: Sprite = null
+music.play(music.melodyPlayable(music.powerUp), music.PlaybackMode.UntilDone)
 game.splash("Dino Runner", "AGER")
 scene.setBackgroundImage(assets.image`Background`)
- 
-tiles.setCurrentTilemap(tilemap`foreground`)
+tiles.setCurrentTilemap(tilemap`level2`)
+music.play(music.stringPlayable("C5 A B G A F G E ", 120), music.PlaybackMode.LoopingInBackground)
+info.setScore(0)
 scroller.scrollBackgroundWithSpeed(-100, 0)
 mySprite = sprites.create(assets.image`Player01`, SpriteKind.Player)
 mySprite.setPosition(26, 65)
-
-info.setScore(0)
 controller.moveSprite(mySprite, 0, 0)
-mySprite.ay = 300
+mySprite.ay = 350
 animation.runImageAnimation(
 mySprite,
 assets.animation`myAnim0`,
 75,
 true
 )
-info.setScore(0)
 forever(function () {
-    projectile = sprites.createProjectileFromSide(assets.image`Cactus01`, -100, 0)
-    projectile.y = 75
+    Cactus = sprites.createProjectileFromSide(assets.image`Cactus1`, -100, 0)
+    Cactus.y = 75
     pause(randint(1000, 4000))
 })
 forever(function () {
-    projectile = sprites.createProjectileFromSide(assets.image`Cactus0`, -100, 0)
-    projectile.y = 75
+    Cactus = sprites.createProjectileFromSide(assets.image`Cactus2`, -100, 0)
+    Cactus.y = 75
+    pause(randint(3000, 7000))
+})
+forever(function () {
+    Cactus = sprites.createProjectileFromSide(assets.image`Cactus0`, -100, 0)
+    Cactus.y = 75
     pause(randint(3000, 4000))
 })
 forever(function () {
-    projectile = sprites.createProjectileFromSide(assets.image`Cactus2`, -100, 0)
-    projectile.y = 75
-    pause(randint(3000, 7000))
+    if (info.score() == 500) {
+        Bat = sprites.createProjectileFromSprite(img`
+            . . f f f . . . . . . . . f f f 
+            . f f c c . . . . . . f c b b c 
+            f f c c . . . . . . f c b b c . 
+            f c f c . . . . . . f b c c c . 
+            f f f c c . c c . f c b b c c . 
+            f f c 3 c c 3 c c f b c b b c . 
+            f f b 3 b c 3 b c f b c c b c . 
+            . c b b b b b b c b b c c c . . 
+            . c 1 b b b 1 b b c c c c . . . 
+            c b b b b b b b b b c c . . . . 
+            c b c b b b c b b b b f . . . . 
+            f b 1 f f f 1 b b b b f c . . . 
+            f b b b b b b b b b b f c c . . 
+            . f b b b b b b b b c f . . . . 
+            . . f b b b b b b c f . . . . . 
+            . . . f f f f f f f . . . . . . 
+            `, mySprite, 50, 45)
+        animation.runImageAnimation(
+        Bat,
+        [img`
+            . . f f f . . . . . . . . f f f 
+            . f f c c . . . . . . f c b b c 
+            f f c c . . . . . . f c b b c . 
+            f c f c . . . . . . f b c c c . 
+            f f f c c . c c . f c b b c c . 
+            f f c 3 c c 3 c c f b c b b c . 
+            f f b 3 b c 3 b c f b c c b c . 
+            . c b b b b b b c b b c c c . . 
+            . c 1 b b b 1 b b c c c c . . . 
+            c b b b b b b b b b c c . . . . 
+            c b c b b b c b b b b f . . . . 
+            f b 1 f f f 1 b b b b f c . . . 
+            f b b b b b b b b b b f c c . . 
+            . f b b b b b b b b c f . . . . 
+            . . f b b b b b b c f . . . . . 
+            . . . f f f f f f f . . . . . . 
+            `,img`
+            . . f f f . . . . . . . . . . . 
+            f f f c c . . . . . . . . f f f 
+            f f c c . . c c . . . f c b b c 
+            f f c 3 c c 3 c c f f b b b c . 
+            f f b 3 b c 3 b c f b b c c c . 
+            . c b b b b b b c f b c b c c . 
+            . c b b b b b b c b b c b b c . 
+            c b 1 b b b 1 b b b c c c b c . 
+            c b b b b b b b b c c c c c . . 
+            f b c b b b c b b b b f c . . . 
+            f b 1 f f f 1 b b b b f c c . . 
+            . f b b b b b b b b c f . . . . 
+            . . f b b b b b b c f . . . . . 
+            . . . f f f f f f f . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `,img`
+            . . . . . . . . . . . . . . . . 
+            . . c c . . c c . . . . . . . . 
+            . . c 3 c c 3 c c c . . . . . . 
+            . c b 3 b c 3 b c c c . . . . . 
+            . c b b b b b b b b f f . . . . 
+            c c b b b b b b b b f f . . . . 
+            c b 1 b b b 1 b b c f f f . . . 
+            c b b b b b b b b f f f f . . . 
+            f b c b b b c b c c b b b . . . 
+            f b 1 f f f 1 b f c c c c . . . 
+            . f b b b b b b f b b c c . . . 
+            c c f b b b b b c c b b c . . . 
+            c c c f f f f f f c c b b c . . 
+            . c c c . . . . . . c c c c c . 
+            . . c c c . . . . . . . c c c c 
+            . . . . . . . . . . . . . . . . 
+            `,img`
+            . f f f . . . . . . . . f f f . 
+            f f c . . . . . . . f c b b c . 
+            f c c . . . . . . f c b b c . . 
+            c f . . . . . . . f b c c c . . 
+            c f f . . . . . f f b b c c . . 
+            f f f c c . c c f b c b b c . . 
+            f f f c c c c c f b c c b c . . 
+            . f c 3 c c 3 b c b c c c . . . 
+            . c b 3 b c 3 b b c c c c . . . 
+            c c b b b b b b b b c c . . . . 
+            c b 1 b b b 1 b b b b f c . . . 
+            f b b b b b b b b b b f c c . . 
+            f b c b b b c b b b b f . . . . 
+            . f 1 f f f 1 b b b c f . . . . 
+            . . f b b b b b b c f . . . . . 
+            . . . f f f f f f f . . . . . . 
+            `],
+        25,
+        true
+        )
+    }
+})
+forever(function () {
+    while (Cactus || Bat.x == 0) {
+        sprites.destroy(mySprite)
+    }
 })
 game.onUpdateInterval(100, function () {
     info.changeScoreBy(1)
