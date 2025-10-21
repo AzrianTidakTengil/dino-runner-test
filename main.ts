6fa51b3237d60c5e23ab543a126e64a3fe00fdb2
Hikoyu <71440999+AzrianTidakTengil@users.noreply.github.com>
2025-10-21 14:57:24 +0700

    Initial with palette pastal

--- main.ts
+++ main.ts
@@ -1,1 +1,27 @@
-
+let projectile: Sprite = null
+scene.setBackgroundImage(assets.image`Background`)
+scroller.scrollBackgroundWithSpeed(-50, 0)
+let mySprite = sprites.create(assets.image`Player01`, SpriteKind.Player)
+mySprite.setPosition(26, 75)
+info.setScore(0)
+animation.runImageAnimation(
+mySprite,
+assets.animation`myAnim0`,
+200,
+true
+)
+forever(function () {
+    projectile = sprites.createProjectileFromSide(assets.image`Cactus01`, -50, 0)
+    projectile.y = 75
+    pause(2000)
+})
+forever(function () {
+    projectile = sprites.createProjectileFromSide(assets.image`Cactus0`, -50, 0)
+    projectile.y = 75
+    pause(2000)
+})
+forever(function () {
+    projectile = sprites.createProjectileFromSide(assets.image`Cactus2`, -50, 0)
+    projectile.y = 75
+    pause(2000)
+})
